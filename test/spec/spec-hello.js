1603dc8014fe30d1a41a290ae5f66a3b44ef999d
Sangwon Lee <gamzabaw@gmail.com>
2015-07-16 16:31:46 +0900

    boilerplate

---
+++ test/spec/spec-hello.js
@@ -0,0 +1,10 @@
+describe('Hello Test', function () {
+	describe('Should Hello', function () {
+		beforeEach(function () {
+		});
+
+		it('Hello should be Hello', function () {
+			expect('Hello').toBe('Hello');
+		});
+	});
+});
